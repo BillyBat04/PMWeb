752d5edfcecb62a468daa2ec1604cf939ae1cedf
BillyBat04 <khoafd11@gmail.com>
2024-10-10 19:11:53 +0700

    tạo ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,17 @@ namespace PMWeb.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            // Lấy chuỗi kết nối từ file Web.config
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PMDataConnectionString"].ConnectionString;
+
+            // Khởi tạo đối tượng QuanLySanPhamDataContext với constructor có tham số chuỗi kết nối
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionString);
+
+            // Lấy danh sách Catalog từ cơ sở dữ liệu
+            List<Product> dsProducts = context.Products.ToList();
+
+            // Trả về view với danh sách Catalog
+            return View(dsProducts);
         }
     }
 }
